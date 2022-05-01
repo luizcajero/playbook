05b07d71f2a904bce38693933cd0d51e441f6837
Luis Felipe Cajero <99145300+luizcajero@users.noreply.github.com>
2022-05-01 15:17:01 -0500

    Create properties_object.js

---
+++ weekly_mission_2/Object/properties_object.js
@@ -0,0 +1,7 @@
+//Ejemplo 2: Creando un objeto con propiedades / Example 2: Creating an object with properties
+const mySecondObject = {
+    name:"Luis Felipe",
+    age: 23 
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades / Example 2: Create an object with properties")
+console.log(mySecondObject)
