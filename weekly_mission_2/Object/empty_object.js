00ee2e2abac36377c7c1973b916db4617b316110
Luis Felipe Cajero <99145300+luizcajero@users.noreply.github.com>
2022-05-01 14:55:45 -0500

    Add files via upload

---
+++ weekly_mission_2/Object/empty_object.js
@@ -0,0 +1,4 @@
+//creando un objeto vacío/ Creating an empty object
+const myFirstObject = {} //esto es un objeto vacío/This is an empty object
+console.log("Ejemplo 1: Crear un objeto vacio/Example 1: Create en empty object")
+console.log(myFirstObject)
