2e413fac73c34a8427a4ad34aa9323004597ac9b
Luis Felipe Cajero <99145300+luizcajero@users.noreply.github.com>
2022-05-02 21:33:07 -0500

    Create someproperties.js

---
+++ weekly_mission_2/Object/someproperties.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades / Example 3: Object with different properties
+const myThirdObjetct = {
+    name: "Kanek",
+    age: 1,
+    nicknames : [
+        "Kaneksito",
+        "Guaponek",
+        "Lobito"
+    ],
+    address: {
+        zip_code: "07800",
+        street: "Av. Fortuna 106 GAM",
+        city: "CDMX"
+    }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades / Example 3: Object with different properties")
+console.log(myThirdObjetct)
+console.log(myThirdObjetct.name)
+console.log(myThirdObjetct["address"])
