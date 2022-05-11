8f9f84845b268de6f71d0e81052df03db2de3286
Luis Felipe Cajero <99145300+luizcajero@users.noreply.github.com>
2022-05-10 23:29:36 -0500

    Update for_each3.js

--- weekly_mission_2/Operators/Example3/for_each3.js
+++ weekly_mission_2/Operators/Example3/for_each3.js
@@ -1,3 +1,4 @@
+// Ejemplo 3: forEach para imprimir los países en letras mayúsculas / for Each to print the countries in capital letters
 const contries = ['Finland', 'Norway', 'Spain', 'France', 'United States', 'Mexico', 'Canada']
 console.log("Ejemplo 3: Imprimiendo la lista de paises en mayúscula")
 contries.forEach((element) => console.log(element.toUpperCase()))
